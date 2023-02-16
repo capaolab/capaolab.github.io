972feaa70e7db21135b747d02a7dccddbc8d47a9
Viriato Sampaio <viriatosampaio@gmail.com>
2023-02-16 15:38:42 -0300

    ajust next config and script

--- capaolab/next.config.js
+++ capaolab/next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
   assetPrefix: isProd ? process.env.NEXT_PUBLIC_BASE_PATH : '',
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
   reactStrictMode: true,
   images: {
     remotePatterns: [
