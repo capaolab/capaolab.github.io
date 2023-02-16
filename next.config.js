92f17093d83a0f64977ca4a8f1a56326b1935b9b
Dean Almeida <dean.informatica@gmail.com>
2023-02-16 16:32:48 -0300

    ajustando developer

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  assetPrefix: isProd ? process.env.NEXT_PUBLIC_BASE_PATH : '',
   reactStrictMode: true,
   images: {
     remotePatterns: [
