a371577eecc4ffee508b3c423f86484d15441f76
Dean Almeida <dean.informatica@gmail.com>
2023-02-17 01:44:30 -0300

    responsivo mobile fix01

--- src/components/effects_components/Carousel.jsx
+++ src/components/effects_components/Carousel.jsx
@@ -41,7 +41,7 @@ const Carousel = () => {
                     <p>Previous</p>
                 </button>
             </div>
-            <div className="md:hidden w-full h-full absolute top-[700px] z-20 flex justify-center items-start">
+            <div className="md:hidden w-full h-full absolute top-[80%] z-20 flex justify-center items-start">
                 <button className="w-40 h-auto px-4 py-2 space-x-4 flex justify-center items-center text-white  hover:text-green-500" onClick={handlePrevious}>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 12h-15m0 0l6.75 6.75M4.5 12l6.75-6.75" />
