b485ad88f45ca7e459d376772130bcc6d8b97db8
viriato sampaio <viriatosampaio@gmail.com>
2024-10-29 09:17:04 -0300

    feat: updating e2e tests

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,5 +4,5 @@
     const title = "capao lab"
 </script>
 <section class="w-full h-full relative snap-always snap-start bg-terracota-75">
-    <h1 class="text-3xl">{capitalizeWord(title)}</h1>
+    <h1 data-testid="title" class="text-3xl">{capitalizeWord(title)}</h1>
 </section>
